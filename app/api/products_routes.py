c98dccd24086efceca59040ba34632b80d13f2b4
allen <allenfaughn@gmail.com>
2022-08-04 12:19:52 -0500

    added C/R/U/D routes for products

--- app/api/products_routes.py
+++ app/api/products_routes.py
@@ -18,6 +18,72 @@ def all_products():
     products = Product.query.all()
     return products.to_dict()
 
+#GET - Get the new product Form
+#POST - Create new product
+@product_router.route('/new', methods=['GET','POST'])
+def new_product():
+    form = NewProductForm()
+    form ['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        product = Product(
+            user_id = form.data['user_id'],
+            name = form.data['name'],
+            price = form.data['price'],
+            description = form.data['description'],
+            weapon_type = form.data['weapon_type'],
+            base_damage = form.data['base_damage'],
+            scaling_type = form.data['scaling_type'],
+            can_be_buffed = form.data['can_be_buffed'],
+            posted = form.data['posted']
+        )
+    if form.errors:
+        print(form.errors)
+        return {'errors': validation_errors_to_error_messages(form.errors)}, 400
+    db.session.add(product)
+    db.session.commit()
+    return product.to_dict()
+
+#GET an individual product's details
+@product_router.route('/<productId>')
+def singleProduct(productId):
+    product = Product.query.get(productId)
+    return product.to_dict()
+
+#DELETEs an individual product
+@product_router.route('/<productId>/delete', methods=['DELETE'])
+def deleteProduct(productId):
+    product = Product.query.get(productId)
+    db.session.delete(product)
+    db.session.commit()
+    return product.to_dict()
+
+
+#PUT updated info to a product
+@product_router.route('/<productId>/edit', methods=['PUT'])
+def editRestaurant(productId):
+    product = Product.query.get(productId)
+    form= NewProductForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    print(form.data['name'])
+    if(form.validate_on_submit()):
+        product.name= form.data['name']
+        product.price = form.data['price']
+        product.description = form.data['description']
+        product.weapon_type = form.data['weapon_type']
+        product.base_damage = form.data['base_damage']
+        product.scaling_type = form.data['scaling_type']
+        product.can_be_buffed = form.data['can_be_buffed']
+
+    if form.errors:
+        print(form.errors)
+        return {'errors': validation_errors_to_error_messages(form.errors)}, 400
+
+    db.session.commit()
+
+    return product.to_dict()
+
+
+#Test route
 @product_router.route('/test')
 def all_products_test():
     products = Product.query.all()
