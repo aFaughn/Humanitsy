85b76c8f3c80b3b64a531bf81149ba9a7567a4ef
allen <allenfaughn@gmail.com>
2022-12-09 21:55:22 -0600

    adding frontend section to query products by their weapon_type

---
+++ react-app/src/components/products/CategorizedWeapons.js
@@ -0,0 +1,23 @@
+import './CategorizedWeapons.css';
+
+function CategorizedWeapons() {
+    return (
+        <div>
+            <h1 id='category-section-title'>Weapon Categories</h1>
+            <ul id='categories-ul'>
+                <li className='category-listing'>Greatswords</li>
+                <li className='category-listing'>Straight Swords</li>
+                <li className='category-listing'>Curved Swords</li>
+                <li className='category-listing'>Halberds</li>
+                <li className='category-listing'>Daggers</li>
+                <li className='category-listing'>Bows</li>
+                <li className='category-listing'>Throwables</li>
+                <li className='category-listing'>Axes</li>
+                <li className='category-listing'>Polearms</li>
+                <li className='category-listing'>Greatswords</li>
+            </ul>
+        </div>
+    )
+}
+
+export default CategorizedWeapons;
