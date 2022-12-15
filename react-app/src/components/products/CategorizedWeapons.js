9a8ef5adfdcb5d4d8f25d77238fc6753cfbedc5d
allen <allenfaughn@gmail.com>
2022-12-14 21:34:24 -0600

    adjusted titling, added placeholder texT

--- react-app/src/components/products/CategorizedWeapons.js
+++ react-app/src/components/products/CategorizedWeapons.js
@@ -25,15 +25,23 @@ function CategorizedWeapons() {
 
 
     return (
-        <div id='cat-container'>
-            <h1 id='category-section-title'>Weapon Categories</h1>
-            <ul id='categories-ul'>
-                <li id='cat-scroll'><FontAwesomeIcon icon={faArrowLeft} onClick={() => catScroll === 0 ? setCatScroll(categories.length - 1) : setCatScroll(catScroll - 1)}/></li>
-                {categories[catScroll].map(item => (
-                    <li className='category-listing'>{item}</li>
-                ))}
-                <li id='cat-scroll'><FontAwesomeIcon icon={faArrowRight} onClick={() => catScroll === categories.length - 1 ? setCatScroll(0) : setCatScroll(catScroll + 1)}/></li>
-            </ul>
+        <div>
+            <div id='cat-container'>
+                <h1 id='category-section-title'>Product Categories</h1>
+                <ul id='categories-ul'>
+                    <li id='cat-scroll'><FontAwesomeIcon icon={faArrowLeft} onClick={() => catScroll === 0 ? setCatScroll(categories.length - 1) : setCatScroll(catScroll - 1)}/></li>
+                    {categories[catScroll].map(item => (
+                        <li className='category-listing'>{item}</li>
+                        ))}
+                    <li id='cat-scroll'><FontAwesomeIcon icon={faArrowRight} onClick={() => catScroll === categories.length - 1 ? setCatScroll(0) : setCatScroll(catScroll + 1)}/></li>
+                </ul>
+            </div>
+            <div id='filtered-prod-container'>
+                    <div id='filt-card-container'>
+                        <p style={{color: 'white'}}>!PLACEHOLDER! Query for products by category selected, map them here.</p>
+                        <p style={{color: 'white'}}>More seed data might work well here :P</p>
+                    </div>
+            </div>
         </div>
     )
 }
