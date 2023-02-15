349dc8341b37e576b1609c01beadf8f503cb051c
allen <allenfaughn@gmail.com>
2023-02-15 16:36:06 -0600

    removed disable css button, added mobile styling

--- react-app/src/components/SplashPage.js
+++ react-app/src/components/SplashPage.js
@@ -15,6 +15,7 @@ function SplashPage() {
     const [showLoginModal, setShowLoginModal] = useState(false)
     const [showSignupModal, setShowSignupModal] = useState(false)
 
+
     useEffect(() => {
 
     }, [showLoginModal, showSignupModal])
@@ -22,22 +23,22 @@ function SplashPage() {
     return (
         <>
         <div id='flybox'>
-            <img  id='fly1' src='https://darksouls3.wiki.fextralife.com/file/Dark-Souls-3/old_wolf_curved_sword.png' class='flyby-image'/>
-            <img  id='fly2' src='https://darksouls3.wiki.fextralife.com/file/Dark-Souls-3/brigand_twindaggers.png' class='flyby-image'/>
-            <img  id='fly3' src='https://darksouls3.wiki.fextralife.com/file/Dark-Souls-3/dragonslayer_greataxe.png' class='flyby-image'/>
-            <img  id='fly4' src='https://darksouls3.wiki.fextralife.com/file/Dark-Souls-3/black_knight_sword.png' class='flyby-image'/>
-            <img  id='fly5' src='https://darksouls3.wiki.fextralife.com/file/Dark-Souls-3/ring_of_favor.png' class='flyby-image'/>
-            <img  id='fly6' src='https://darksouls3.wiki.fextralife.com/file/Dark-Souls-3/black_firebomb.png' class='flyby-image'/>
+            <img  id='fly1' src='https://darksouls3.wiki.fextralife.com/file/Dark-Souls-3/old_wolf_curved_sword.png' className='flyby-image' alt='animation'/>
+            <img  id='fly2' src='https://darksouls3.wiki.fextralife.com/file/Dark-Souls-3/brigand_twindaggers.png' className='flyby-image' alt='animation'/>
+            <img  id='fly3' src='https://darksouls3.wiki.fextralife.com/file/Dark-Souls-3/dragonslayer_greataxe.png' className='flyby-image' alt='animation'/>
+            <img  id='fly4' src='https://darksouls3.wiki.fextralife.com/file/Dark-Souls-3/black_knight_sword.png' className='flyby-image' alt='animation'/>
+            <img  id='fly5' src='https://darksouls3.wiki.fextralife.com/file/Dark-Souls-3/ring_of_favor.png' className='flyby-image' alt='animation'/>
+            <img  id='fly6' src='https://darksouls3.wiki.fextralife.com/file/Dark-Souls-3/black_firebomb.png' className='flyby-image' alt='animation'/>
         </div>
             <div id='splash-content-wrapper'>
                 <h1 id='splash-header'>Humanitsy</h1>
                 <div id='weapon-spin-wrapper'>
                     <div id='weapon-spin-container'>
                         <div id='weapon-spin-image-container'>
-                            <img class='weapon-image' id='wimg1' src='https://darksouls3.wiki.fextralife.com/file/Dark-Souls-3/zweihander.png'/>
-                            <img class='weapon-image' id='wimg2' src='https://darksouls3.wiki.fextralife.com/file/Dark-Souls-3/morion_blade.png'/>
-                            <img class='weapon-image' id='wimg3' src='https://darksouls3.wiki.fextralife.com/file/Dark-Souls-3/exile_greatsword.png'/>
-                            <img class='weapon-image' id='wimg4' src="https://darksouls3.wiki.fextralife.com/file/Dark-Souls-3/ledo's_great_hammer.png"/>
+                            <img className='weapon-image' id='wimg1' src='https://darksouls3.wiki.fextralife.com/file/Dark-Souls-3/zweihander.png' alt='animation'/>
+                            <img className='weapon-image' id='wimg2' src='https://darksouls3.wiki.fextralife.com/file/Dark-Souls-3/morion_blade.png' alt='animation'/>
+                            <img className='weapon-image' id='wimg3' src='https://darksouls3.wiki.fextralife.com/file/Dark-Souls-3/exile_greatsword.png' alt='animation'/>
+                            <img className='weapon-image' id='wimg4' src="https://darksouls3.wiki.fextralife.com/file/Dark-Souls-3/ledo's_great_hammer.png" alt='animation'/>
                         </div>
                     </div>
                 </div>
@@ -77,7 +78,6 @@ function SplashPage() {
               </div>
             </div>
             )}
-            <div id='disable-animation'>Disable CSS Animation</div>
         </>
     )
 }
