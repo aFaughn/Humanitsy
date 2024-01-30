58a1232d650d4c3179b8f2ee641f888e03584ec0
aFaughn <allenfaughn@gmail.com>
2024-01-30 17:35:51 -0600

    styled footer

--- react-app/src/components/Footer.js
+++ react-app/src/components/Footer.js
@@ -2,7 +2,10 @@ import './Footer.css'
 
 function Footer() {
     return (
-        <div id='footer_test'>Footer Component</div>
+        <div id='footer_test'>
+            <i>I know not what you seek in this far-away land, But I pray for your safety. -Lucatiel</i><br/><br/>
+            This app was built by Allen Faughn @ <a href='https://www.github.com/afaughn'>Github</a> & <a href='https://linkedin.com/in/allen-faughn'>LinkedIn</a>
+        </div>
     )
 }
 
